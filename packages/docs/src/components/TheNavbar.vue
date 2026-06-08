8e29bad6ed64cb330fafc523d0b610a2122f4a08
Wesley Ford <wes@wesdevpro.com>
2026-06-08 14:50:03 -0400

    refactor(docs): replace manual navbar with b-navbar component  A few things to check in the browser after this:

--- packages/docs/src/components/TheNavbar.vue
+++ packages/docs/src/components/TheNavbar.vue
@@ -1,157 +1,129 @@
 <template>
-    <nav
-        class="navbar docs-navbar is-spaced has-shadow"
-        :class="{ 'is-primary is-transparent': isLight, 'is-scrolled': isScrolled }"
+    <b-navbar
+        ref="navbar"
+        v-model="isMenuOpen"
+        class="docs-navbar"
+        :type="isLight ? 'is-primary' : ''"
+        :transparent="isLight"
+        wrapper-class="container"
+        spaced
+        shadow
     >
-        <div class="container">
-            <div class="navbar-brand">
-                <router-link
-                    to="/"
-                    class="navbar-item"
-                    title="Buefy: lightweight UI components for Vue.js based on Bulma"
-                    active-class=""
+        <template #brand>
+            <b-navbar-item
+                tag="router-link"
+                to="/"
+                active-class=""
+                title="Buefy: lightweight UI components for Vue.js based on Bulma"
+            >
+                <img
+                    v-if="isLight"
+                    src="../assets/buefy-light.png"
+                    alt="Buefy"
                 >
-                    <img
-                        v-if="isLight"
-                        src="../assets/buefy-light.png"
-                        alt="Buefy"
-                    >
-                    <img
-                        v-else
-                        src="../assets/buefy.png"
-                        alt="Buefy"
-                    >
+                <img
+                    v-else
+                    src="../assets/buefy.png"
+                    alt="Buefy"
+                >
+            </b-navbar-item>
+
+            <b-navbar-item
+                tag="a"
+                href="https://github.com/buefy/buefy"
+                target="_blank"
+                title="Github"
+            >
+                <b-icon icon="github" />
+            </b-navbar-item>
+
+            <b-navbar-item
+                tag="a"
+                href="https://discord.buefy.org/"
+                target="_blank"
+                title="Discord"
+            >
+                <b-icon icon="discord" />
+            </b-navbar-item>
+
+            <b-navbar-item
+                tag="a"
+                href="https://x.com/buefycss"
+                target="_blank"
+                title="Twitter"
+            >
+                <b-icon
+                    pack="fa"
+                    custom-class="fa-brands"
+                    icon="x-twitter"
+                />
+            </b-navbar-item>
+        </template>
+
+        <template #end>
+            <b-navbar-item tag="router-link" to="/" exact>
+                Home
+            </b-navbar-item>
+
+            <b-navbar-item tag="router-link" to="/documentation">
+                Documentation
+            </b-navbar-item>
+
+            <b-navbar-item tag="router-link" to="/extensions">
+                Extensions
+            </b-navbar-item>
+
+            <b-navbar-item tag="router-link" to="/expo">
+                Expo
+            </b-navbar-item>
+
+            <b-navbar-dropdown label="Info" hoverable>
+                <strong class="navbar-item is-version">
+                    <span class="has-text-primary">Buefy version</span>
+                    <span class="has-text-grey">{{ version }}</span>
+                </strong>
+                <strong class="navbar-item is-version">
+                    <span class="has-text-bulma">Bulma version</span>
+                    <span class="has-text-grey">{{ bulmaVersion }}</span>
+                </strong>
+                <hr class="navbar-divider">
+                <b-navbar-item
+                    tag="a"
+                    href="https://github.com/buefy/buefy/releases"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    Changelogs
+                </b-navbar-item>
+            </b-navbar-dropdown>
+
+            <b-navbar-item tag="div">
+                <router-link to="/documentation/start" class="navbar-cta">
+                    Get Started
                 </router-link>
+            </b-navbar-item>
 
-                <a
-                    class="navbar-item"
-                    href="https://github.com/buefy/buefy"
-                    target="_blank"
-                    title="Github"
-                >
-                    <b-icon icon="github" />
-                </a>
-
-                <a
-                    class="navbar-item"
-                    href="https://discord.buefy.org/"
-                    target="_blank"
-                    title="Discord"
-                >
-                    <b-icon icon="discord" />
-                </a>
-
-                <a
-                    class="navbar-item"
-                    href="https://x.com/buefycss"
-                    target="_blank"
-                    title="Twitter"
-                >
-                    <b-icon
-                        pack="fa"
-                        custom-class="fa-brands"
-                        icon="x-twitter"
-                    />
-                </a>
-
-                <span
-                    class="navbar-burger burger"
-                    :class="{ 'is-active': isMenuActive }"
-                    @click="isMenuActive = !isMenuActive"
-                >
-                    <span />
-                    <span />
-                    <span />
-                    <span />
-                </span>
-            </div>
-
-            <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
-                <div class="navbar-end">
-                    <router-link
-                        to="/"
-                        exact
-                        class="navbar-item"
-                    >
-                        Home
-                    </router-link>
-
-                    <router-link to="/documentation" class="navbar-item">
-                        Documentation
-                    </router-link>
-
-                    <router-link to="/extensions" class="navbar-item">
-                        Extensions
-                    </router-link>
-
-                    <router-link to="/expo" class="navbar-item">
-                        Expo
-                    </router-link>
-
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <div class="navbar-link">
-                            Info
-                        </div>
-
-                        <div class="navbar-dropdown">
-                            <strong class="navbar-item is-version">
-                                <span class="has-text-primary">Buefy version</span>
-                                <span class="has-text-grey">{{ version }}</span>
-                            </strong>
-                            <strong class="navbar-item is-version">
-                                <span class="has-text-bulma">Bulma version</span>
-                                <span class="has-text-grey">{{
-                                    bulmaVersion
-                                }}</span>
-                            </strong>
-
-                            <hr class="navbar-divider">
-                            <div class="navbar-item">
-                                <a
-                                    href="https://github.com/buefy/buefy/releases"
-                                    target="_blank"
-                                    rel="noopener"
-                                >
-                                    Changelogs
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="navbar-item">
-                        <router-link
-                            to="/documentation/start"
-                            class="navbar-cta"
-                        >
-                            Get Started
-                        </router-link>
-                    </div>
-
-                    <div class="navbar-item">
-                        <ThemeToggle />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
+            <b-navbar-item tag="div">
+                <ThemeToggle />
+            </b-navbar-item>
+        </template>
+    </b-navbar>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { BNavbar, BNavbarItem, BNavbarDropdown, BIcon } from 'buefy'
 
-import { BIcon } from 'buefy'
 import ThemeToggle from '@/components/ThemeToggle.vue'
-
 import buefyPackage from '../../../../package.json'
 import bulmaPackage from 'bulma/package.json'
 import { useTheme } from '@/composables/useTheme'
 
 export default defineComponent({
-    components: { BIcon, ThemeToggle },
+    components: { BNavbar, BNavbarItem, BNavbarDropdown, BIcon, ThemeToggle },
     data() {
         return {
-            isMenuActive: false,
-            isScrolled: false,
+            isMenuOpen: false,
             isLight: useTheme().isLight,
             version: buefyPackage.version,
             bulmaVersion: bulmaPackage.version
@@ -159,20 +131,26 @@ export default defineComponent({
     },
     methods: {
         closeMenu() {
-            this.isMenuActive = false
+            this.isMenuOpen = false
         }
     },
     mounted() {
         this.$eventHub.on('navigate', this.closeMenu)
-        this._onScroll = () => { this.isScrolled = window.scrollY > 60 }
+
+        // Apply scroll-blur class directly on the underlying <nav> element
+        // since BNavbar uses a render function (class fallthrough isn't guaranteed)
+        const navEl = (this.$refs.navbar as InstanceType<typeof BNavbar>).$el as HTMLElement
+        navEl.classList.add('docs-navbar')
+        this._onScroll = () => {
+            navEl.classList.toggle('is-scrolled', window.scrollY > 60)
+        }
         window.addEventListener('scroll', this._onScroll, { passive: true })
+
         useTheme().setTheme(useTheme().getTheme())
     },
-
     beforeUnmount() {
         this.$eventHub.off('navigate', this.closeMenu)
         window.removeEventListener('scroll', this._onScroll)
-        document.documentElement.classList.remove('is-clipped-touch')
     }
 })
 </script>
